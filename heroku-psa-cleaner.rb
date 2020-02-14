1361e17560798e048765fcd9a9d50faf7756e204
Greg Nokes <tsykoduk@gmail.com>
2020-02-14 10:35:09 -0800

    major updates, will correctly handle locked and spaces apps now

--- heroku-psa-cleaner.rb
+++ heroku-psa-cleaner.rb
@@ -6,23 +6,28 @@
 # If you wish to not get prompted for each one (DANGEROUS), just comment out the `gets` loop
 
 
-TEAMS_TO_CLEAN =  ["lit-beach"] # ["heroku-psa"] ["heroku-se-demo"] ["heroku-training"] #
+teams_to_clean = "heroku-se-demo", "heroku-psa"
 apps = []
 
-for team in TEAMS_TO_CLEAN do
+for team in teams_to_clean do
   apps.push(*`heroku apps --team #{team}`.split(/\n+/).drop(1))
 end
 
 for app in apps
-  puts "deleting app: #{app}, are you sure? (y/n)"
-  quest = gets
-  if quest.chr == "y"
-    puts "deleting app #{app}"
-    `heroku destroy --app #{app} --confirm #{app}`
-     puts "We just deleted app #{app}"
+  if app.include?("locked")
+    puts "#{app} is a locked app, skipping"
   else
-    puts "skipping app #{app}"
-  end
+    puts "deleting app: #{app}, are you sure? (y/n)"
+    # quest = gets
+    # if quest.chr == "y"
+      puts "deleting app #{app}"
+      a = app.split(" ")
+      `heroku destroy --app #{a[0]} --confirm #{a[0]}`
+       puts "We just deleted app #{app}"
+       # else
+       #   puts "skipping app #{app}"
+       # end
+     end
 end
 
   
